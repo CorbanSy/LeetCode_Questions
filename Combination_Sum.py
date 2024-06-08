e8e0fe992b0b6af215286f20b197f5869d1678c5
CorbanSy <corbansy10@gmail.com>
2024-06-07 22:06:03 -0700

    I finished the combination sum leet code question

---
+++ Combination_Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, current_combination, current_sum):
+            if current_sum == target:
+                result.append(list(current_combination))
+                return
+            if current_sum > target:
+                return
+            
+            for i in range(start, len(candidates)):
+                current_combination.append(candidates[i])
+                backtrack(i, current_combination, current_sum + candidates[i])
+                current_combination.pop()
+        
+        result = []
+        candidates.sort()
+        backtrack(0, [], 0)
+        return result
